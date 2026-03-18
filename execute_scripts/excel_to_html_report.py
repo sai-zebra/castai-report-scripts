0e4b83971dd0aa496aa5b23fe31e198c6fc4854d
DS5985 <donakonda.saikrishna@zebra.com>
2026-03-18 15:12:23 +0530

    excel_html

--- execute_scripts/excel_to_html_report.py
+++ execute_scripts/excel_to_html_report.py
@@ -14,7 +14,7 @@ sheet_names = list(sheets.keys())
 html_content = f"""
 <html>
 <head>
-<title>CAST AI Cluster Report</title>
+<title>CAST AI Clusters Report</title>
 <style>
  
 body{{
@@ -94,19 +94,47 @@ padding:8px;
 margin:5px;
 border-radius:5px;
 }}
+
+.header-container{{
+position:sticky;
+top:0;
+z-index:1000;
+background:#f4f6f9;
+padding-bottom:10px;
+}}
+
+.search-box{{
+text-align:center;
+margin:10px;
+}}
+
+input[type="text"]{{
+padding:10px;
+width:40%;
+border-radius:5px;
+border:1px solid #ccc;
+}}
  
 </style>
 </head>
 <body>
  
-<h1>CAST AI Cluster Report</h1>
+<div class="header-container">
+<h1>CAST AI Clusters Report</h1>
+
+<div class="search-box">
+<input type="text" id="searchInput"
+placeholder="Search By ClusterName..."
+onkeyup="searchTable()">
+    </div>
+</div>
  
 """
  
 # Create pages
 for i, (sheet_name, df) in enumerate(sheets.items()):
     html_content += f'<div class="page" id="page{i}">'
-    html_content += f"<h2>{sheet_name}</h2>"
+    html_content += f'<h2 style="position:sticky;top:80px;z-index:999;">{sheet_name}</h2>'
     html_content += df.to_html(index=False)
     html_content += "</div>"
  
@@ -172,6 +200,53 @@ function goToPage() {{
     showPage(currentPage);
 }}
  
+//Search Function
+function searchTable(){{
+ let input = document.getElementById("searchInput").value.toLowerCase();
+
+ for (let p=0; p<totalPages; p++){{
+ let page = document.getElementById("page"+p);
+ let rows = page.getElementsByTagName("tr");
+
+ let pageHasMatch = false;
+
+ for (let i=1; i<rows.length; i++){{
+ let cells = rows[i].getElementsByTagName("td");
+ let rowText="";
+
+ for (let j=0; j<cells.length; j++){{
+ rowText += cells[j].innerText.toLowerCase()+"";
+ }}
+
+ if (rowText.includes(input)){{
+ rows[i].style.display="";
+ pageHasMatch=true;
+ }} else{{
+ rows[i].style.display="none";
+ }}
+ }}
+
+ //jump to page where match found
+ if (pageHasMatch && input !== ""){{
+ currentPage = p;
+ showPage(currentPage);
+ }}
+ }}
+
+ //reset if empty
+ if (input === ""){{
+ for (let p=0; p<totalPages; p++){{
+ let page = document.getElementById("Page" + p);
+ let rows = page.getElementsByTagName("tr");
+
+ for (let i=1; i<rows.length; i++){{
+ rows[i].style.display = "";
+ }}
+ }}
+ showPage(currentPage);
+ }}
+}}
+
 showPage(currentPage);
  
 </script>
